d85cace9766ca6851e5d87a84e698f3b79452545
ashraf <ashraf1abdelrasool@gmail.com>
2023-01-17 17:29:56 +0200

    initial commit

--- src/test/java/com/asrevo/cacheerror/CacheErrorApplicationTests.java
+++ src/test/java/com/asrevo/cacheerror/CacheErrorApplicationTests.java
@@ -1,10 +1,10 @@
-package com.asrevo.gatewayerror;
+package com.asrevo.cacheerror;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayErrorApplicationTests {
+class CacheErrorApplicationTests {
 
 	@Test
 	void contextLoads() {
